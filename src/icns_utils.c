7976cb3f4699e0418d1f499be703939434718836
Mathew Eis <mathew@eisbox.net>
2008-06-12 09:51:34 +0000

    Fixed incorrect FSF address, updated ChangeLog

--- src/icns_utils.c
+++ src/icns_utils.c
@@ -3,7 +3,7 @@ File:       icns_utils.c
 Copyright (C) 2001-2008 Mathew Eis <mathew@eisbox.net>
 
 This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public
+modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2 of the License, or (at your option) any later version.
 
@@ -14,8 +14,8 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+Boston, MA 02110-1301, USA.
 */
 
 #include <stdio.h>
